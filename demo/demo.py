b8aa8092362983dccb2485aeee2f713e732be1ea
Nate Stevens, PhD <33462727+nts345045@users.noreply.github.com>
2024-11-04 14:08:54 -0800

    Initial commit

---
+++ demo/demo.py
@@ -0,0 +1,92 @@
+"""
+:module: demo.py
+:auth: Nathan T. Stevens
+:email: ntsteven@uw.edu
+:org: Pacific Northwest Seismic Network
+:license: GNU GPLv3
+:purpose: This script provides an example template construction
+    and waveform cross-correlation clustering routine using most
+    of the methods provided in this repository. 
+
+    This particular example uses a set of "well-located" low
+    frequency seismic events from Mt. Baker as the input 
+    catalog events.
+
+"""
+
+# Provides a demonstration of an example workflow using `eqc_utils`
+import os
+from obspy import read_events
+from obspy.clients.fdsn import Client
+from eqcorrscan import Template, Tribe
+
+from eqc_utils.logging import setup_terminal_logger
+from eqc_utils.eventbank import initialize_event_bank
+from eqc_utils.catalog import apply_phase_hints, filter_picks
+from eqc_utils.template import augment_template, rename_templates
+from eqc_utils.clustering import cluster
+from eqc_utils.snuffler import plant
+
+# Add 'snuffler' functionality to Tribe and Template class objects
+plant()
+
+# Set up command-line print-out for logging
+Logger = setup_terminal_logger(__name__)
+
+# Set up an IRIS webservices client
+client = Client("IRIS")
+Logger.info('')
+# Load example event metadata from this repo
+cat = read_events(os.path.join('.','mt_baker_catalog.xml'))
+
+# Create an event bank & add catalog to it
+ebank = initialize_event_bank(
+    catalog=cat,
+    base_path=os.path.join('..','demo_output','eventbank'),
+    path_structure='{year}')                    
+
+# Add phase hints from arrivals to picks
+cat = apply_phase_hints(cat)
+
+# Subset down to 3 nearest stations to Mt. Baker & only keep P picks
+cat = filter_picks(
+    catalog=cat,
+    stations=['MBW','MBW2','SHUK'],
+    phase_hints=['P'])
+
+# Create a tribe of templates using IRIS webservices for waveforms
+tribe = Tribe().construct(
+    method='from_client',
+    client_id=client,
+    catalog=cat,
+    samp_rate=50.,
+    lowcut=0.5,
+    highcut=20.,
+    filt_order=4.,
+    prepick=5.,
+    length=40.,
+    delayed=True,
+    process_len=3600.,
+    parallel=True,
+    num_cores=6
+)
+
+# Rename the templates with EVID's including contributor codes
+tribe = rename_templates(tribe)
+
+# Augment the templates to have 3-C data (if applicable)
+for template in tribe:
+    template = augment_template(template, client)
+
+# Visualize the tribe with snuffler
+tribe.snuffle()
+
+# Run clustering on templates
+groups = cluster(
+    tribe,
+    corr_thresh=0.4,
+    fill_value=1,
+    show=True,
+    save_subtribes=True,
+    save_path=os.path.join('..','demo_output','template_clustering'))
+
---
+++ eqc_utils/snuffler.py
@@ -0,0 +1,84 @@
+"""
+:module: eqc_utils.snuffler
+:auth: Nathan T. Stevens
+:email: ntsteven@uw.edu
+:org: Pacific Northwest Seismic Network
+:license: GNU GPLv3
+:purpose:
+    This module extends the pyrocko.obspy_compat.plant method
+    to also allow EQcorrscan Template and Tribe objects to be
+    visualized with `snuffler`, automaticaly including the
+    event information as marker(s) and scaling the maximum
+    number of visible traces to the number of unique trace ID's
+    present in either object's waveform data.
+"""
+
+from obspy import Catalog, Stream, Inventory
+from pyrocko import obspy_compat
+
+def plant():
+    """
+    Runs the :meth:`~pyrocko.obspy_compat.plant` method and extends
+    it to also add the `snuffle` method to EQcorrscan Template and Tribe
+    class objects.
+    """    
+    obspy_compat.plant()
+    import eqcorrscan
+    eqcorrscan.Tribe.snuffle = snuffle_tribe
+    eqcorrscan.Template.snuffle = snuffle_template
+
+def snuffle_template(template, **kwargs):
+    """
+    Initialize a Pyrocko "snuffler" GUI minimally displaying the
+    traces and event marker contained in this :class:`~eqcorrscan.core.match_filter.template.Template`
+    object's **st** and **event** attributes.
+
+    Note: adding an inventory for relevant stations will allow distance sorting!
+
+    
+
+    :return: (return_tag, markers)
+    :rtype: (str, list[pyrocko markers] )
+    """
+    if 'ntracks' not in kwargs.keys():
+        kwargs.update({'ntracks': len({tr.id for tr in template.st})})
+    if 'catalog' not in kwargs.keys():
+        kwargs.update({'catalog': Catalog(events=[template.event])})
+    return template.st.snuffle(catalog=Catalog([template.event]), **kwargs)
+
+def snuffle_tribe(tribe, **kwargs):
+    """Initialize a Pyrocko "snuffler" GUI instance
+    for the contents of this :class:`~eqcorrscan.core.match_filter.tribe.Tribe`
+    minimally displaying waveform data for all **template.st** and event
+    markers for all **template.event** in this tribe
+
+    :param tribe: tribe to snuffle
+    :type tribe: eqcorrscan.core.match_filter.tribe.Tribe
+    :return: (return_tag, markers)
+    :rtype: (str, list[pyrocko markers] )
+    """    
+    big_st = Stream()
+    cat = Catalog()
+    for temp in tribe:
+        big_st += temp.st
+        cat.events.append(temp.event)
+
+    # If ntracks is not provided, set ntracks to the number of unique channel IDs
+    if 'ntracks' not in kwargs.keys():
+        kwargs.update({'ntracks': len({tr.id for tr in big_st})})
+    
+    # If a catalog is provided
+    if 'catalog' in kwargs.keys():
+        # If it does not perfectly match the catalog comprised of template events
+        if kwargs['catalog'] != cat:
+            # Add them together
+            kwargs['catalog'] += cat
+        # Otherwise, catalog provided matches, don't duplicate
+        else:
+            pass
+    # If no catalog is provided, add this to kwargs
+    else:
+        kwargs.update({'catalog': cat})
+    # Run instance and capture output
+    return big_st.snuffle( **kwargs)
+
