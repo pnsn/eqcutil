7c5cd38e18b7c68bb10b8a66c5ef47cfe6eb9c10
Nate Stevens <33462727+nts345045@users.noreply.github.com>
2024-11-08 12:09:50 -0800

    rename

--- eqcutil/catalog/model_phases.py
+++ eqcutil/catalog/model_phases.py
@@ -17,66 +17,6 @@ from obspy import Inventory
 from obspy.core.event import Pick, Arrival, WaveformStreamID
 from obspy.geodetics import locations2degrees
 from pyrocko import cake
-from eqcutil.util.logging import rich_error_message
-
-class RayModeler(object):
-    """
-    A class for managing the inputs and outputs of modeling 
-    """
-    def __init__(self, model='P4', inventory=None):
-        # Set up logging
-        self.Logger = logging.getLogger(self.__name__())
-        # Compatability checks on model
-        if isinstance(model, cake.LayeredModel):
-            self.model = model
-        elif isinstance(model, str):
-            try:
-                self.make_model(name=model)
-            except Exception as e:
-                self.Logger.critical(rich_error_message(e))
-        else:
-            self.Logger.critical(f'TypeError: "model" value {model} not supported.')
-        
-        # inventory compatability checks
-        if isinstance(inventory, Inventory):
-            self.inv = inventory
-            if len(self.inv) == 0:
-                self.Logger.warning('Initialized with an empty inventory - will need to add stations to model rays!')
-        elif inventory is None:
-            self.Logger.warning('Initialized without an inventory - will need to add an inventory to model rays!')
-        else:
-            self.Logger.critical(f'TypeError: "inventory" type {type(inventory)} not supported.')
-        # Initialize Summary Object
-        self.summary = pd.DataFrame()
-        # Initialize origin list
-        self.origins = []
-
-    def __name__(self):
-        return 'eqcorrscan_utils.cake.RayModeler'
-
-    def make_model(self, name='P4'):
-        try:
-            self.model = make_model(name=name)
-        except Exception as e:
-            raise
-
-    def model_raypaths(self, origin, phases=['P','S']):
-        if not (origin, phases) in self.origins:
-            results = model_raypaths(self.model, origin, self.inv, phases=phases)
-            results = ray_summary(results, origin, self.inv)
-            self.summary = pd.concat([self.summary, results], ignore_index=True)
-            self.origins.append((origin, phases))
-        else:
-            self.Logger.warning('This origin-phases combination has already been run!')
-    
-
-
-    # def 
-
-
-#######################
-### METHODS SECTION ###
-#######################
 
 def model_arrivals(origin, inventory, model_name='P4', phases=['P','S']):
     """Model raypaths and travel-times between a seismic event
