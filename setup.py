f87d3ebba33165fb0c689f630584e6a8921ddbd1
Nate Stevens, PhD <33462727+nts345045@users.noreply.github.com>
2024-11-15 14:18:26 -0800

    Update docstring

--- setup.py
+++ setup.py
@@ -1,8 +1,6 @@
 """
-PULSE is a open-source project that builds on the PyEarthworm, ObsPy, Numpy, and SeisBench APIs to facilitate
-testing and operationalization of Python-based machine learning workflows that use Earthworm messages as inputs
-and outputs
-
+eqcutil is a open-source project that adds functionalities to eqcorrscan based
+analyses
 :copyright: Nathan T. Stevens and Pacific Northwest Seismic Network (pnsn.org)
 :license: AGPL-3.0
 """
