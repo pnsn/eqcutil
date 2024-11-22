deaddf9887aa8aac6023a5955682385a35bb8547
Nate Stevens, PhD <33462727+nts345045@users.noreply.github.com>
2024-11-22 14:48:18 -0800

    Integrate submethod, add docs

--- eqcutil/io/quakemigrate.py
+++ eqcutil/io/quakemigrate.py
@@ -10,7 +10,7 @@
 :AI Attribution: This module was prototyped using prompts with ChatGPT and then
     tested and verified for functionality by the author
 """
-import os, logging
+import os, logging, warnings
 
 import pandas as pd
 from obspy import UTCDateTime
@@ -53,7 +53,7 @@ PICK_FILE_COLS = [
 
 # TODO: Make a stream formatter that accepts and Inventory as an input
 
-def stream_id_formatter(phase, station, network='UW', location='', phase_mapping={'P':'HHZ','S':'HHN'}):
+def stream_id_formatter(phase, station, network='UW', location='', chan_mapping={'P':'HHZ','S':'HHN'}):
     """Given the Phase and Station values from a row in a *.pick file,
     reconstitute a SEED channel ID string
 
@@ -65,14 +65,89 @@ def stream_id_formatter(phase, station, network='UW', location='', phase_mapping
     :type network: str, optional
     :param location: default location code, defaults to ''
     :type location: str, optional
-    :param phase_mapping: phase to channel code mapping, defaults to {'P':'HHZ','S':'HHN'}
-    :type phase_mapping: dict, optional
+    :param chan_mapping: phase to channel code mapping, defaults to {'P':'HHZ','S':'HHN'}
+    :type chan_mapping: dict, optional
     :return: SEED channel ID
     :rtype: str
     """    
-    return '%s.%s.%s.%s'%(network, station, location, phase_mapping[phase])
+    if phase not in chan_mapping.keys():
+        raise KeyError('Phase value not found in chan_mapping keys')
+    if phase not in ['P','S']:
+        raise ValueError('Phase must be either "P" or "S"')
+    if chan_mapping.keys() != set(['P','S']):
+        raise KeyError('chan_mapping keys must be "P" and "S"')
+    if not isinstance(station, (str, int)):
+        raise ValueError('station must be type str or int')
+    if not isinstance(location, str):
+        raise ValueError('location must be type str')
+    return '%s.%s.%s.%s'%(network, station, location, chan_mapping[phase])
 
-def  quakemigrate2cat(event_files, pick_files, hyp_type='max', min_snr=3, stream_id_formatter=stream_id_formatter):
+def resource_id_formatter(format='quakeml', source='local', method='quakemigrate', resource_type='event', extras=None, resource_name=None):
+    """Convenience method wrapping :class:`~obspy.core.event.resourceid.ResourceIdentifier`
+
+    Provides fields and defaults for formatting
+
+    :param format: Identifier format, defaults to 'quakeml'
+        Supports: 'quakeml' and 'smi'
+    :type format: str, optional
+    :param source: name of where the metadata came from, defaults to 'local'
+        'local' is a catch-all for stuff you ran on your computer
+    :type source: str, optional
+    :param method: what method/software was used to generate these metadata,
+        defaults to 'quakemigrage'
+    :type method: str, optional
+    :param resource_type: type of metadata, defaults to 'event'
+        Expected values: event, origin, pick, arrival, magnitude
+    :type resource_type: str, optional
+    :param resource_name: unique identifier for this resource, defaults to None
+        None - allows ResourceIdentifier to assign a unique hash-key using the
+         **prefix** input argument to ResourceIdentifier
+        str or int - string or integer to use as the unique (string formatted)
+            resource ID
+    :type resource_name: None, str, int, optional
+    :return: resource_id object
+    :rtype: obspy.core.event.resourceid.ResourceIdentifier
+    """    
+    # input compatability checks
+    if format not in ['quakeml','smi']:
+        raise ValueError(f'format "{format}" not supported')
+    if not isinstance(source, str):
+        raise TypeError
+    if not isinstance(method, str):
+        raise TypeError
+    if not isinstance(resource_type, str):
+        raise TypeError
+    elif resource_type not in ['event','origin','pick','arrival','magnitude']:
+        warnings.warn(f'resource_type "{resource_type}" is non-standard')
+
+    # Create prefix
+    prefix = f'{format}:{source}/{method}/{resource_type}'
+    if isinstance(extras, str):
+        prefix = f'{prefix}/{extras}'
+    elif hasattr(extras, '__iter__'):
+        if all(isinstance(_e, str) for _e in extras):
+            for _e in extras:
+                prefix = f'{prefix}/{_e}'
+    elif extras is None:
+        pass
+    else:
+        raise ValueError('extras must be str, an iterable collection of str, or NoneType')
+
+    # If resource_name is provided, pass as **id** to ResourceID
+    if isinstance(resource_name, (str,int)):
+        rid=ResourceIdentifier(id=f'{prefix}/{resource_name}')
+    # If resource_name is None, use **prefix** 
+    elif resource_name is None:
+        rid=ResourceIdentifier(prefix=prefix)
+    # Otherwise, raise error
+    else:
+        raise TypeError('resource_name must be type int, str, or NoneType')
+    # Return ResourceIdentifier object
+    return rid
+
+def  quakemigrate2cat(event_files, pick_files, hyp_type='max', min_snr=3,
+                      network='XX', location='', chan_mapping={'P': 'HHZ','S':'HHN'},
+                      qml_extras=None):
     """Convert the output *.event and *.pick files from a QuakeMigrate
     run into an ObsPy :class:`~obspy.core.event.Catalog` object that
     has the necessary pick/arrival referencing and phase name / hint
@@ -92,7 +167,15 @@ def  quakemigrate2cat(event_files, pick_files, hyp_type='max', min_snr=3, stream
     :param min_snr: minimum Signal to Noise Ratio to allow for QuakeMigrate picks (column SNR),
         defaults to 3
     :type min_snr: scalar, optional.
-
+    :param network: network code to assign to all picks to make a SEED channel name code,
+        defaults to 'XX'
+    :type network: str, optional
+    :param location: location code to assign to all picks to make a SEED channel name code,
+         defaults to ''
+    :type location: str, optional
+    :param chan_mapping: phase name to SEED channel code mapping, defaults to {'P': 'HHZ', 'S':'HHN'},
+    :type chan_mapping: dict, optional
+    
     :return: catalog-formatted event metadata
     :rtype: obspy.core.event.Catalog
     
@@ -105,6 +188,9 @@ def  quakemigrate2cat(event_files, pick_files, hyp_type='max', min_snr=3, stream
     the prescribed earth model could be added. These items are not strictly
     needed if the user simply wants to convert the QM outputs into a "bare-minimum"
     catalog 
+
+    TODO: eventually allow an obspy.core.inventory.Inventory object input
+    for the SEED channel naming function
     """    
 
     # Compatability check for event_file
@@ -143,12 +229,98 @@ def  quakemigrate2cat(event_files, pick_files, hyp_type='max', min_snr=3, stream
     else:
         Logger.critical(f'pick_files type {type(pick_files)} not supported. Must be str or list of str')    
     
+    # Check if any of the events have local magnitude estimates
+    if 'ML' in df_e.columns:
+        hasmag = True
+    else:
+        hasmag = False
 
-    cat = _qm2cat_inner_process(df_e, df_p, hyp_type=hyp_type, min_snr=min_snr, formatter=stream_id_formatter)
+    # Sanity check to only take picks that match the given Event_ID
+    df_p = df_p[df_p.EventID.isin(df_e.EventID)]
+    if len(df_p) == 0:
+        Logger.critical('No phases matched event_id values in "event_file"')
+    else:
+        Logger.info(f'Matched {len(df_p)} picks to {len(df_e)} events')
 
+    ## START MAKING THE CATALOG ##
+    cat = Catalog()
+    for _, erow in df_e.iterrows():
+        # Subset Picks to Match current EVID
+        Logger.info(f'Processing event_id: {erow.EventID}')
+        idf_picks = df_p[df_p.EventID == erow.EventID]
+        Logger.info(f'...with {len(idf_picks)} picks')
+        # Create event
+        event = Event(resource_id = resource_id_formatter(extras=qml_extras, resource_name=erow.EventID))
+        # Create Origin
+        origin = Origin(resource_id = resource_id_formatter(extras=qml_extras, resource_type='origin'))
+
+        # Populate best-estimate hypocenter
+        origin.time = UTCDateTime(erow.DT)
+        if hyp_type.lower() == 'max':
+            origin.latitude = erow.Y
+            origin.longitude = erow.X
+            origin.depth = erow.Z
+        # Or populate with gaussian hypocenter
+        elif hyp_type.lower() == 'gau': 
+            origin.latitude = erow.GAU_Y
+            origin.longitude = erow.GAU_X
+            origin.depth = erow.GAU_Z
+
+
+        # TODO: Add uncertainties (need to do coordinate conversions)
+
+        # Append origin to event
+        event.origins.append(origin)
+        # Set as preferred origin ID
+        event.preferred_origin_id = origin.resource_id
+
+        if hasmag:
+            if isinstance(erow.ML, (int, float)):   
+                Logger.info(f'EVID: {erow.EventID} has magnitude estimate - including in Event description')
+                magnitude = Magnitude(
+                    resource_id=resource_id_formatter(extras=qml_extras, resource_type='magnitude'),
+                    mag=erow.ML,
+                    magnitude_type='ML',
+                    mag_errors=QuantityError(uncertainty=erow.ML_Err),
+                    origin_id = origin.resource_id
+                                    )
+                # Append magnitude to event
+                event.magnitudes.append(magnitude)
+                # Set as preferred magnitude ID
+                event.preferred_magnitude_id = magnitude.resource_id
+            else:
+                Logger.info(f'EVID: {erow.EventID} did not have magnitude estimate - skipping magnitude object generation')
+
+        # Populate Picks and Arrivals
+        for _, prow in idf_picks.iterrows():
+            # Create pick
+            if prow.SNR >= min_snr:
+                seed_id = stream_id_formatter(prow.Phase, prow.Station, network=network, location=location, chan_mapping=chan_mapping)
+                pick = Pick(
+                    resource_id=resource_id_formatter(extras=qml_extras, resource_type='pick'),
+                    time = UTCDateTime(prow.PickTime),
+                    time_errors = prow.PickError,
+                    waveform_id = WaveformStreamID(seed_string=seed_id),
+                    evaluation_mode = 'automatic',
+                    phase_hint=prow.Phase)
+                # Create arrival that references pick and has travel time uncertainty
+                arrival = Arrival(
+                    resource_id=resource_id_formatter(extras=qml_extras, resource_type='arrival'),
+                    pick_id = pick.resource_id,
+                    phase=prow.Phase,
+                    time_residual=UTCDateTime(prow.PickTime) - UTCDateTime(prow.ModelledTime))
+                # Append pick to event
+                event.picks.append(pick)
+                # Append arrival to preferred origin
+                event.preferred_origin().arrivals.append(arrival)
+            else:
+                continue
+        # Append event to catalog
+        cat.events.append(event)
+    # Return catalog
     return cat
 
-def _qm2cat_inner_process(df_e, df_p, hyp_type='max', min_snr=3, formatter=stream_id_formatter):
+def _qm2cat_inner_process(df_e, df_p, hyp_type='max', min_snr=3, network='', location='', chan_mapping={'P':'HHZ','S':'HHN'}):
     """PRIVATE METHOD
 
     Inner method for converting pre-read & format checked
@@ -176,87 +348,7 @@ def _qm2cat_inner_process(df_e, df_p, hyp_type='max', min_snr=3, formatter=strea
     if hyp_type.lower() not in ['max','gau']:
         Logger.critical(f'hyp_type {hyp_type} not supported.')
 
-    # Check if any of the events have local magnitude estimates
-    if 'ML' in df_e.columns:
-        hasmag = True
-    else:
-        hasmag = False
-
-    # Sanity check to only take picks that match the given Event_ID
-    df_p = df_p[df_p.EventID.isin(df_e.EventID)]
-    if len(df_p) == 0:
-        Logger.critical('No phases matched event_id values in "event_file"')
-    else:
-        Logger.info(f'Matched {len(df_p)} picks to {len(df_e)} events')
-
-    ## START MAKING THE CATALOG ##
-    cat = Catalog()
-    for _, erow in df_e.iterrows():
-        # Subset Picks to Match current EVID
-        Logger.info(f'Processing event_id: {erow.EventID}')
-        idf_picks = df_p[df_p.EventID == erow.EventID]
-        Logger.info(f'...with {len(idf_picks)} picks')
-        # Create event
-        event = Event()
-        # Create Origin
-        origin = Origin()
-        # Populate best-estimate hypocenter
-        origin.time = UTCDateTime(erow.DT)
-        if hyp_type.lower() == 'max':
-            origin.latitude = erow.Y
-            origin.longitude = erow.X
-            origin.depth = erow.Z
-        elif hyp_type.lower() == 'gau': 
-            origin.latitude = erow.GAU_Y
-            origin.longitude = erow.GAU_X
-            origin.depth = erow.GAU_Z
-
-
-        # TODO: Add uncertainties (need to do coordinate conversions)
-
-        # Append origin to event
-        event.origins.append(origin)
-        # Set as preferred origin ID
-        event.preferred_origin_id = origin.resource_id
-
-        if hasmag:
-            if isinstance(erow.ML, (int, float)):   
-                Logger.info(f'EVID: {erow.EventID} has magnitude estimate - including in Event description')
-                magnitude = Magnitude(mag=erow.ML,
-                                    magnitude_type='ML',
-                                    mag_errors=QuantityError(uncertainty=erow.ML_Err),
-                                    origin_id = origin.resource_id
-                                    )
-                # Append magnitude to event
-                event.magnitudes.append(magnitude)
-                # Set as preferred magnitude ID
-                event.preferred_magnitude_id = magnitude.resource_id
-            else:
-                Logger.info(f'EVID: {erow.EventID} did not have magnitude estimate - skipping magnitude object generation')
-
-        # Populate Picks and Arrivals
-        for _, prow in idf_picks.iterrows():
-            # Create pick
-            if prow.SNR >= min_snr:
-                seed_id = formatter(prow.Phase, prow.Station)
-                pick = Pick(time = UTCDateTime(prow.PickTime),
-                            time_errors = UTCDateTime(prow.PickError),
-                            waveform_id = WaveformStreamID(seed_string=seed_id),
-                            evaluation_mode = 'automatic',
-                            phase_hint=prow.Phase)
-                # Create arrival that references pick and has travel time uncertainty
-                arrival = Arrival(pick_id = pick.resource_id,
-                                phase=prow.Phase,
-                                time_residual=UTCDateTime(prow.PickTime) - UTCDateTime(prow.ModelledTime))
-                # Append pick to event
-                event.picks.append(pick)
-                # Append arrival to preferred origin
-                event.preferred_origin().arrivals.append(arrival)
-            else:
-                continue
-        # Append event to catalog
-        cat.events.append(event)
-    # Return catalog
+    
     return cat
 
         
